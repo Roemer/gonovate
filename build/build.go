16bb9f6e7ffbb6e476c542733f14a984e6753ac6
Roemer <r.baeriswyl@gmail.com>
2024-05-28 10:57:33 +0200

    Add ci

--- build/build.go
+++ build/build.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"archive/zip"
+	"context"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/google/go-github/v62/github"
 	"github.com/roemer/gotaskr"
 	"github.com/roemer/gotaskr/execr"
+	"github.com/roemer/gotaskr/log"
 )
 
 // Internal variables
@@ -20,6 +24,14 @@ func main() {
 }
 
 func init() {
+	gotaskr.Task("Compile:All", func() error {
+		return nil
+	}).
+		DependsOn("Compile:Windows").
+		DependsOn("Compile:Linux").
+		DependsOn("Compile:Mac").
+		DependsOn("Compile:MacArm")
+
 	gotaskr.Task("Compile:Windows", func() error {
 		os.Setenv("GOOS", "windows")
 		os.Setenv("GOARCH", "amd64")
@@ -63,6 +75,52 @@ func init() {
 		}
 		return zipRelease(path)
 	})
+
+	gotaskr.Task("Release", func() error {
+		log.Informationf("Creating new release for version %s", version)
+		gitHubRepoParts := strings.Split(os.Getenv("GITHUB_REPOSITORY"), "/")
+		gitHubOwner := gitHubRepoParts[0]
+		gitHubRepo := gitHubRepoParts[1]
+		gitHubToken := os.Getenv("GITHUB_TOKEN")
+
+		// Create the client
+		ctx := context.Background()
+		client := github.NewClient(nil).WithAuthToken(gitHubToken)
+
+		// Create the new release
+		newRelease := &github.RepositoryRelease{
+			Name:    github.String(version),
+			Draft:   github.Bool(true),
+			TagName: github.String(version),
+		}
+		release, _, err := client.Repositories.CreateRelease(ctx, gitHubOwner, gitHubRepo, newRelease)
+		if err != nil {
+			return err
+		}
+		log.Informationf("Created release: %s", *release.URL)
+
+		// Upload the artifacts
+		artifacts, err := os.ReadDir(outputDirectory)
+		if err != nil {
+			return err
+		}
+		for _, artifactPath := range artifacts {
+			log.Informationf("Uploading artifact %s", artifactPath.Name())
+			f, err := os.Open(filepath.Join(outputDirectory, artifactPath.Name()))
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			_, _, err = client.Repositories.UploadReleaseAsset(ctx, gitHubOwner, gitHubRepo, *release.ID, &github.UploadOptions{
+				Name: artifactPath.Name(),
+			}, f)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
 }
 
 func compile(ext string) (string, error) {
@@ -79,7 +137,10 @@ func zipRelease(file string) error {
 	}
 	defer a.Close()
 
-	return createFlatZip(a, file)
+	if err := createFlatZip(a, file); err != nil {
+		return err
+	}
+	return os.Remove(file)
 }
 
 func createFlatZip(w io.Writer, files ...string) error {
