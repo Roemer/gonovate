9e767a7489ed0ce11449377b6e05ccd48f6a73e5
Roemer <r.baeriswyl@gmail.com>
2025-04-30 10:25:03 +0200

    Added go version test

--- internal/integration_tests/datasource_test.go
+++ internal/integration_tests/datasource_test.go
@@ -130,3 +130,45 @@ func TestGitLabPackages(t *testing.T) {
 	fmt.Println("Update found to version:")
 	fmt.Println(ri.VersionString)
 }
+
+func TestGoVersion(t *testing.T) {
+	t.Skip("This test is for local debugging only")
+
+	assert := assert.New(t)
+
+	// Load the defaults
+	cfg, err := config.Load("preset:defaults")
+	assert.NoError(err)
+	assert.NotNil(cfg)
+
+	// Create the datasource
+	settings := &common.DatasourceSettings{
+		Logger:    slog.Default(),
+		HostRules: cfg.HostRules,
+	}
+	ds := datasources.NewGoVersionDatasource(settings)
+
+	// Create the dependency and enrich it with rules from the config
+	dep := &common.Dependency{Name: "go-stable", Datasource: common.DATASOURCE_TYPE_GOVERSION, Version: "1.23.0"}
+	dep.MaxUpdateType = common.UPDATE_TYPE_PATCH
+	//dep.ExtractVersion
+	dep.IgnoreNonMatching = common.TruePtr
+	err = cfg.ApplyToDependency(dep)
+	assert.NoError(err)
+
+	// Get the releases from the datasource
+	releases, err := ds.GetReleases(dep)
+	assert.NoError(err)
+	assert.NotNil(releases)
+	fmt.Println("Found releases:")
+	for _, release := range releases {
+		fmt.Println(release.VersionString)
+	}
+
+	// Search for an update
+	ri, err := ds.SearchDependencyUpdate(dep)
+	assert.NoError(err)
+	assert.NotNil(ri)
+	fmt.Println("Update found to version:")
+	fmt.Println(ri.VersionString)
+}
